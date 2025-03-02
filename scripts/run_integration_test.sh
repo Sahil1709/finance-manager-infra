6411eea56d9f5d78eb57c7977f11592862a88b1c
sahil <sahilofficial17@gmail.com>
2025-03-02 14:09:22 -0800

    Enhance integration test script by adding database connection checks and improving container management

--- scripts/run_integration_test.sh
+++ scripts/run_integration_test.sh
@@ -35,6 +35,10 @@ docker pull ${ECR_REGISTRY}/${BACKEND_IMAGE}:latest
 # Extract database password and database name from DATABASE_URL
 DATABASE_PASSWORD=$(echo $DATABASE_URL | sed -n 's/.*:\/\/.*:\(.*\)@.*/\1/p')
 DATABASE_NAME=$(echo $DATABASE_URL | sed -n 's/.*:\/\/.*\/\(.*\)/\1/p')
+DATABASE_HOST="localhost"
+export DATABASE_URL="mysql://root:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}"
+# FOr testing
+echo "DATABASE_URL: $DATABASE_URL"
 
 # Run containers using the updated images.
 echo "Starting new containers..."
@@ -42,12 +46,26 @@ docker run -d --name frontend -p 3000:3000 ${ECR_REGISTRY}/${FRONTEND_IMAGE}:lat
 docker run -d --name backend -p 8000:8000 ${ECR_REGISTRY}/${BACKEND_IMAGE}:latest
 
 # Start MySQL container with extracted database password and database name
-# docker run --name mysql-8 -e MYSQL_ROOT_PASSWORD=$DATABASE_PASSWORD -e MYSQL_DATABASE=$DATABASE_NAME -d mysql:8
+docker run --name mysql-8 -e MYSQL_ROOT_PASSWORD=$DATABASE_PASSWORD -e MYSQL_DATABASE=$DATABASE_NAME -d mysql:8
 
 echo "Test Deployment complete!"
 
 # Wait for containers to fully start.
-sleep 30
+sleep 10
+
+# Check if containers are running.
+docker ps -a
+
+if ! docker ps | grep frontend && ! docker ps | grep backend; then
+  echo "Containers are not running."
+  exit 2
+fi
+
+echo "Testing database connection..."
+mysql -h "${DATABASE_URL}" -u root -p"${DATABASE_PASSWORD}" -e "USE ${DATABASE_NAME}; SELECT 1;" || {
+    echo "Database connection failed"
+    exit 3
+}
 
 # Run smoke tests.
 FRONTEND_RESULT=$(curl -s http://localhost:3000/)
@@ -57,12 +75,12 @@ if echo "$FRONTEND_RESULT" | grep "Finance Manager" && echo "$BACKEND_RESULT" | 
   echo "SUCCESS: Smoke tests passed."
   echo "SUCCESS" > /tmp/smoke_test_result.txt
   echo "::set-output name=result::success"
-  docker compose down
+  docker rm -f frontend backend || true
   exit 0
 else
   echo "FAILURE: Smoke tests did not pass."
   echo "FAILURE: Smoke tests did not pass." > /tmp/smoke_test_result.txt
   echo "::set-output name=result::failure"
-  docker compose down
+  docker rm -f frontend backend || true
   exit 1
 fi

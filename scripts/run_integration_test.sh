ed1223fd2ab8a9e5157091046950968fc39e7a02
sahil <sahilofficial17@gmail.com>
2025-04-29 20:03:01 -0700

    Add GROQ_API_KEY to environment variables and update smoke test conditions

--- scripts/run_integration_test.sh
+++ scripts/run_integration_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-export DATABASE_URL=$DATABASE_URL
-export FRONTEND_URL=$FRONTEND_URL
-export MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
-export MYSQL_DATABASE=$MYSQL_DATABASE
-export NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=$NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
-export CLERK_SECRET_KEY=$CLERK_SECRET_KEY
-export NEXT_PUBLIC_BACKEND_URL=$NEXT_PUBLIC_BACKEND_URL
+# export DATABASE_URL=$DATABASE_URL
+# export FRONTEND_URL=$FRONTEND_URL
+# export MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD
+# export MYSQL_DATABASE=$MYSQL_DATABASE
+# export NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=$NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY
+# export CLERK_SECRET_KEY=$CLERK_SECRET_KEY
+# export NEXT_PUBLIC_BACKEND_URL=$NEXT_PUBLIC_BACKEND_URL
 
 
 cd finance-manager
@@ -19,6 +19,15 @@ cat <<EOF > .env
 DATABASE_URL=$DATABASE_URL
 FRONTEND_URL=$FRONTEND_URL
 FRONTEND_RC_URL=$FRONTEND_RC_URL
+EOF
+
+cd ../analytics_service
+echo "Creating .env file for analytics service"
+cat <<EOF > .env
+DATABASE_URL=$DATABASE_URL
+FRONTEND_URL=$FRONTEND_URL
+FRONTEND_RC_URL=$FRONTEND_RC_URL
+GROQ_API_KEY=$GROQ_API_KEY
 EOF
 
 cd ../frontend
@@ -43,8 +52,11 @@ docker ps -a
 # Run smoke tests.
 FRONTEND_RESULT=$(curl -s http://localhost:3000/)
 BACKEND_RESULT=$(curl -s http://localhost:8000/health/)
+ANALYTICS_RESULT=$(curl -s http://localhost:8001/insights/1234/)
 
-if echo "$FRONTEND_RESULT" | grep "Finance Manager" && echo "$BACKEND_RESULT" | grep healthy; then
+if echo "$FRONTEND_RESULT" | grep "Finance Manager" \
+  && echo "$BACKEND_RESULT" | grep healthy \
+  && echo "$ANALYTICS_RESULT" | grep "No transactions" ; then
   echo "SUCCESS: Smoke tests passed."
   echo "SUCCESS" > /tmp/smoke_test_result.txt
   echo "::set-output name=result::success"
